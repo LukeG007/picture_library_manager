19c03ae136c6a3d8d9ab6e96d2d4c4bd609096c4
LukeG007 <luke@gilsinger.net>
2022-07-24 02:31:31 -0400

    Add support for multiple libraries

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 import os
+import json
 
 app = Flask(__name__)
+f = open('libraries.json')
+libraries = json.loads(f.read())
+f.close()
 
 @app.route('/')
 def home():
-    images = os.listdir(os.path.join('static', 'dakota'))
-    return render_template('home.html', images=images)
+    return render_template('home.html', libraries=libraries)
+
+@app.route('/library/<string:id>')
+def library_view(id):
+    if not id in libraries:
+        abort(404)
+    images = os.listdir(os.path.join('static', id))
+    return render_template('library.html', images=images, title=libraries[id])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5473)
