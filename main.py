246a22401b1d1b167dfb30ded94590cc9ea5fc4d
LukeG007 <luke@gilsinger.net>
2022-07-24 03:29:28 -0400

    Replace libraries.json with SQL Database

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from flask import Flask, render_template, abort
 import os
 import json
+import sqlite3
 
 app = Flask(__name__)
-f = open('libraries.json')
-libraries = json.loads(f.read())
-f.close()
+db = sqlite3.connect('db.sqlite')
+cur = db.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS libraries(id TEXT, pretty_name TEXT)')
+cur.close()
+db.commit()
+db.close()
+
+def get_libraries():
+    db = sqlite3.connect('db.sqlite')
+    cur = db.cursor()
+    cur.execute('SELECT * FROM libraries')
+    libraries_raw = cur.fetchall()
+    cur.close()
+    db.close()
+    libraries = {}
+    for library in libraries_raw:
+        libraries[library[0]] = library[1]
+    return libraries
 
 @app.route('/')
 def home():
-    return render_template('home.html', libraries=libraries)
+    return render_template('home.html', libraries=get_libraries())
 
 @app.route('/library/<string:id>')
 def library_view(id):
+    libraries = get_libraries()
     if not id in libraries:
         abort(404)
     images = os.listdir(os.path.join('static', 'libraries', id))
